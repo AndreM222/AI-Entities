af57ae3756dfd2a2b0f4c893924151904b50407d
Andre Mossi <mossiroberto0392@gmail.com>
2025-03-22 14:43:18 -0400

    refactor(free main project class): clenaup the project class and move to a new class for abstraction

---
+++ Source/AIEntity/Movement-Setup/AIMovementData.cpp
@@ -0,0 +1,47 @@
+#include "AIMovementData.h"
+
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "Modules/ModuleManager.h"
+
+void IController_INTF::INTF_Get_DebugInfo(ACharacter*& OutDebugFocusCharacter, bool& OutDebugView, bool& OutShowHUD,
+	bool& OutShowTraces, bool& OutShowDebugShapes, bool& OutShowLayerColors, bool& OutSlomo, bool& OutShowCharacterInfo)
+{
+}
+
+void ICharacter_INTF::INTF_Get_CurrentStates(
+	TEnumAsByte<EMovementMode>& OutPawnMovementMode,
+	MovementState& OutMovementState, MovementState& OutPrevMovementState, MovementAction& OutMovementAction,
+	RotationMode& OutRotationMode, Gait& OutActualGait, Stance& OutActualStance, ViewMode& OutViewMode,
+	OverlayState& OutOverlayState)
+{
+}
+
+void ICharacter_INTF::INTF_Get_EssentialValues(FVector& OutVelocity, FVector& OutAcceleration, FVector& OutMovementInput,
+	bool& OutIsMoving, bool& OutHasMovementInput, double& OutSpeed, double& OutMovementInputAmount,
+	FRotator& OutAimingRotation, double& OutAimYawRate)
+{
+}
+
+void ICharacter_INTF::INTF_Set_MovementState(MovementState NewMovementState)
+{
+}
+
+void ICharacter_INTF::INTF_Set_MovementAction(MovementAction NewMovementAction)
+{
+}
+
+void ICharacter_INTF::INTF_Set_OverlayState(OverlayState NewOverlayState)
+{
+}
+
+void IAnimation_INTF::INTF_Jumped()
+{
+}
+
+void IAnimation_INTF::INTF_SetGroundedEntryState(GroundedEntryState GroundedEntryState)
+{
+}
+
+void IAnimation_INTF::INTF_SetOverlayOverrideState(int32 OverlayOverrideState)
+{
+}
